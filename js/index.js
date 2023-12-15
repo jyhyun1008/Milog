5d9dea594b316b14ba2f7848441a9782e7612c8d
jyhyun1008 <hyun1008@kasi.re.kr>
2023-12-16 06:51:02 +0900

    셋팅에서 textarea 불러와지지 않는 것 수정

--- js/index.js
+++ js/index.js
@@ -1265,7 +1265,7 @@ if (!blog && !page) {
 
     if (token) {
 
-        document.querySelector('#page_content').innerHTML = '<div class="setting_container"><div>블로그 제목:</div><input id="blogTitle" value="'+signedBlogInfo.blogTitle+'"></input><div>블로그 소개:</div><textarea id="blogIntro" value="'+signedBlogInfo.blogIntro+'"></textarea><div>테마 색상:<br>(블로그의 테마 색상을 변경하는 것이 아닌, 여러분의 브라우저에서 보이는 사이트 전체의 테마 색상을 변경하는 것입니다.)</div><input id="blogTheme" value="'+signedBlogInfo.theme+'"></input><div>카테고리:<br>(개행으로 구분합니다. 최소 1개의 카테고리는 남겨 두셔야 글을 작성하실 수 있습니다.)</div><textarea id="category" value="'+signedBlogInfo.category+'"></textarea></div><div class="button" id="settingChange">설정 변경</div>'
+        document.querySelector('#page_content').innerHTML = '<div class="setting_container"><div>블로그 제목:</div><input id="blogTitle" value="'+signedBlogInfo.blogTitle+'"></input><div>블로그 소개:</div><textarea id="blogIntro">'+signedBlogInfo.blogIntro+'</textarea><div>테마 색상:<br>(블로그의 테마 색상을 변경하는 것이 아닌, 여러분의 브라우저에서 보이는 사이트 전체의 테마 색상을 변경하는 것입니다.)</div><input id="blogTheme" value="'+signedBlogInfo.theme+'"></input><div>카테고리:<br>(개행으로 구분합니다. 최소 1개의 카테고리는 남겨 두셔야 글을 작성하실 수 있습니다.)</div><textarea id="category">'+signedBlogInfo.category+'</textarea></div><div class="button" id="settingChange">설정 변경</div>'
 
         var settingChange = document.getElementById('settingChange');
         var blogTitle = document.getElementById('blogTitle').value.replace('`', '&#x60;').replace('"', '&quot;');
